2706059805663437a072a20b6ef98daa4462a0a6
budiy12 <asepbudiyana9a@gmail.com>
2019-05-23 11:23:26 +0700

    update 3

--- db_phecell.sql
+++ db_phecell.sql
@@ -6,7 +6,11 @@ create table if not exists customer(
 );
 -- add customer
 INSERT INTO customer VALUES
-('member1','Mia Khalifa','12345');
+('member1','Wanda Maximof','wm123'),
+('member2','Elisabeth Olsen','eo123'),
+('member3','Mia Khalifa','mk123'),
+('member4','Natasha Romanoff','nr123'),
+('member5','Scarlet Johanson','sj345');
 
 -- *** --
 
@@ -36,9 +40,12 @@ create table if not exists promo(
 );
 -- add produk
 INSERT INTO promo VALUES
+('0000000000000000',0,DEFAULT),
 ('1234567890123456',5000,DEFAULT),
 ('1231231231231231',7000,DEFAULT),
-('4321432143214321',3000,DEFAULT);
+('4321432143214321',3000,DEFAULT),
+('7777777777777777',2000,DEFAULT),
+('9999999999999999',1000,DEFAULT);
 
 -- *** ---
 
@@ -50,19 +57,33 @@ create table if not exists admin(
 );
 -- add produk
 INSERT INTO admin VALUES
-('Budi12','Asep Budiyana M','aku123');
+('Budi12','Asep Budiyana M','aku123'),
+('Difa12','Difa Bagasputra M','kamu123'),
+('Bia12','Bandana Irmal Abdillah','dia123');
 
 -- tabel pesanan
 create table pesanan(
-    id_pesanan int(50) PRIMARY KEY, 
+    id_pesanan char(15) PRIMARY KEY, 
     waktu timestamp, 
     status_pembayaran boolean DEFAULT false, 
     status_pengiriman boolean DEFAULT false, 
     nomor_hp char(12) NOT NULL, 
     nominal int(7), 
-username char(20), id_produk char(4), kd_promo char(16), id_admin char(6), 
+username char(20), id_produk char(4), kd_promo char(16) DEFAULT '0000000000000000', id_admin char(6), 
 FOREIGN KEY (username) REFERENCES customer(username),
 FOREIGN KEY (id_produk) REFERENCES produk(id_produk),
 FOREIGN KEY (kd_promo) REFERENCES promo(kd_promo),
 FOREIGN KEY (id_admin) REFERENCES admin(id_admin)
 );
+-- add pesanan
+INSERT INTO pesanan VALUES
+('123456789012345',DEFAULT,DEFAULT,DEFAULT,'085223922501',10000,'member1','Tel1','1234567890123456',DEFAULT),
+('234567890123456',DEFAULT,DEFAULT,DEFAULT,'085722040358',20000,'member5','Ind2','1231231231231231',DEFAULT),
+('345678901234567',DEFAULT,DEFAULT,DEFAULT,'087781379245',50000,'member4','Xl03','4321432143214321',DEFAULT),
+('456789012345678',DEFAULT,DEFAULT,DEFAULT,'089919815502',100000,'member2','Bol7','7777777777777777',DEFAULT),
+('567890123456789',DEFAULT,DEFAULT,DEFAULT,'085889664243',20000,'member3','Sma6','9999999999999999',DEFAULT),
+('678901234567890',DEFAULT,DEFAULT,DEFAULT,'087721820990',50000,'member5','Tri5',DEFAULT,DEFAULT),
+('789012345678901',DEFAULT,DEFAULT,DEFAULT,'082122720187',70000,'member3','Axi4',DEFAULT,DEFAULT),
+('890123456789012',DEFAULT,DEFAULT,DEFAULT,'081380273478',5000,'member2','Tel1',DEFAULT,DEFAULT),
+('901234567890123',DEFAULT,DEFAULT,DEFAULT,'082120814276',200000,'member1','Xl03',DEFAULT,DEFAULT),
+('012345678901234',DEFAULT,DEFAULT,DEFAULT,'088012348679',40000,'member4','Bol7',DEFAULT,DEFAULT);
--- status_transaksi.php
+++ status_transaksi.php
@@ -2,7 +2,14 @@
 <html>
 <head>
 	<title>Status Pelanggan</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+  <script type="text/javascript" src="jquery.js"></script>
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body class="inapp">
     <?php
@@ -16,32 +23,57 @@
                 echo "Data berhasil di hapus.";
             }
         }
-    ?>
-    <a href="input.php">+ Tambah Data Baru</a>
+		?>
+		  <nav class="navbar navbar-default navbar navbar-fixed-top">
+        <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand">PHE CELL</a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">				
+                        <li class="active"><a>Status Pelanggan <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Tentang Kami</a></li> 
+                        <li><a href="#">Kontak</a></li> 
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a>Hi, Admin!</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>
+    </nav>
+    <br/>
+    <br/>
+    <br/>
+    <a href="add.php">+ Tambah Data Baru</a>
 	<div>
-		<a>Status Pemesanan</a>
-		<table id="outtable">
+		<a class="text-lg-center text-uppercase">Status Pemesan</a>
+		<table class="table table-bordered table-striped table-hover">
 			<thead>
 				<tr>
 					<th class="short">No.</th>
 					<th class="normal">Nama Customer</th>
 					<th class="normal">No. Kontak</th>
 					<th class="normal">Provider</th>
-                    <th class="normal">Nominal</th>
-                    <th class="normal">Harga</th>
-                    <th class="normal">Potongan</th>
-                    <th class="normal">Harga Total</th>
-                    <th class="normal">Status Pembayaran</th>
-                    <th class="normal">Status Pengiriman</th>
-                    <th class="normal">Sunting Transaksi</th>
+          <th class="normal">Nominal</th>
+          <th class="normal">Harga</th>
+        	<th class="normal">Potongan</th>
+          <th class="normal">Harga Total</th>
+          <th class="normal">Status Pembayaran</th>
+          <th class="normal">Status Pengiriman</th>
+          <th class="normal">Sunting Transaksi</th>
 				</tr>
-            </thead>
-            <?php 
+      </thead>
+        			<?php 
                 include "koneksi.php";
                 $query_mysql = mysql_query("SELECT * FROM pesanan NATURAL JOIN customer NATURAL JOIN produk NATURAL JOIN promo")or die(mysql_error());
                 $nomor = 1;
-                // $query_cust = mysql_query("SELECT * FROM customer WHERE customer.username = pesanan.username")or die(mysql_error());
-                // $data_cust = mysql_fetch_array($query_cust);
                 while($data = mysql_fetch_array($query_mysql)){
                 ?>
                 <tr>
@@ -53,8 +85,24 @@
                     <td><?php echo $data['nominal']+$data['harga_tambahan'];?></td>
                     <td><?php echo "-".$data['potongan'];?></td>
                     <td><?php echo $data['nominal']+$data['harga_tambahan']-$data['potongan'];?></td>
-                    <td><?php if($data['status_pembayaran']){echo "LUNAS";} else {echo "BELUM";}?></td>
-                    <td><?php if($data['status_pengiriman']){echo "OK";} else {echo "NO";}?></td>
+                    <td>
+											<div class="kustom-select" style="width:150px;">
+											<select>
+											<option value="0">Belum Bayar</option>
+											<option value="1">Sudah Bayar</option>
+											<option value="0">Belum Bayar</option>
+											</select>
+											</div>
+										</td>
+                    <td>
+											<div class="kustom-select" style="width:150px;">
+											<select>
+											<option value="0">Belum Terkirim</option>
+											<option value="1">Sudah Terkirim</option>
+											<option value="0">Belum Terkirim</option>
+											</select>
+											</div>	
+										</td>
                     <td>
                         <a class="edit" href="edit.php?id=<?php echo $data['id']; ?>">Edit</a> |
                         <a class="hapus" href="hapus.php?id=<?php echo $data['id']; ?>">Hapus</a>					
@@ -100,6 +148,81 @@
 			</tbody> -->
 		</table>
 	   </div>
+	   <script>
+			var x, i, j, selElmnt, a, b, c;
+			/*look for any elements with the class "kustom-select":*/
+			x = document.getElementsByClassName("kustom-select");
+			for (i = 0; i < x.length; i++) {
+			  selElmnt = x[i].getElementsByTagName("select")[0];
+			  /*for each element, create a new DIV that will act as the selected item:*/
+			  a = document.createElement("DIV");
+			  a.setAttribute("class", "select-selected");
+			  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+			  x[i].appendChild(a);
+			  /*for each element, create a new DIV that will contain the option list:*/
+			  b = document.createElement("DIV");
+			  b.setAttribute("class", "select-items select-hide");
+			  for (j = 1; j < selElmnt.length; j++) {
+				/*for each option in the original select element,
+				create a new DIV that will act as an option item:*/
+				c = document.createElement("DIV");
+				c.innerHTML = selElmnt.options[j].innerHTML;
+				c.addEventListener("click", function(e) {
+					/*when an item is clicked, update the original select box,
+					and the selected item:*/
+					var y, i, k, s, h;
+					s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+					h = this.parentNode.previousSibling;
+					for (i = 0; i < s.length; i++) {
+					  if (s.options[i].innerHTML == this.innerHTML) {
+						s.selectedIndex = i;
+						h.innerHTML = this.innerHTML;
+						y = this.parentNode.getElementsByClassName("same-as-selected");
+						for (k = 0; k < y.length; k++) {
+						  y[k].removeAttribute("class");
+						}
+						this.setAttribute("class", "same-as-selected");
+						break;
+					  }
+					}
+					h.click();
+				});
+				b.appendChild(c);
+			  }
+			  x[i].appendChild(b);
+			  a.addEventListener("click", function(e) {
+				  /*when the select box is clicked, close any other select boxes,
+				  and open/close the current select box:*/
+				  e.stopPropagation();
+				  closeAllSelect(this);
+				  this.nextSibling.classList.toggle("select-hide");
+				  this.classList.toggle("select-arrow-active");
+				});
+			}
+			function closeAllSelect(elmnt) {
+			  /*a function that will close all select boxes in the document,
+			  except the current select box:*/
+			  var x, y, i, arrNo = [];
+			  x = document.getElementsByClassName("select-items");
+			  y = document.getElementsByClassName("select-selected");
+			  for (i = 0; i < y.length; i++) {
+				if (elmnt == y[i]) {
+				  arrNo.push(i)
+				} else {
+				  y[i].classList.remove("select-arrow-active");
+				}
+			  }
+			  for (i = 0; i < x.length; i++) {
+				if (arrNo.indexOf(i)) {
+				  x[i].classList.add("select-hide");
+				}
+			  }
+			}
+			/*if the user clicks anywhere outside the select box,
+			then close all select boxes:*/
+			document.addEventListener("click", closeAllSelect);
+		</script>
+	<!--	<script src="bootstrap/bootstrap.min.js"></script>	-->
 </body>
 
 <?php 
